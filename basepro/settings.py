e9287e0eda3dd855a9f2173999000217c46b6e08
Sirius1942 <chen6_9@163.com>
2019-10-31 16:46:58 +0800

    deploy

--- basepro/settings.py
+++ basepro/settings.py
@@ -154,7 +154,6 @@ STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE':10,
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication'
     )
